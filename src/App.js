c33ce4486d540144cd1737fef66d5d6be5702d67
nomand56 <66408464+nomand56@users.noreply.github.com>
2022-02-17 14:55:28 +0500

    Covid Statistics tracker app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import  Cards from './components/Cards'
+
+function App() {
+      return (<div>
+      <NavBar/>,
+      <Cards/>
+      </div> 
+         )}
+export default App;
